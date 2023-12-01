54ad2fb58168fa0b43f26e3ea74ac2f96e64626e
followthedopamine <83159854+followthedopamine@users.noreply.github.com>
2023-12-01 17:43:37 +1300

    Solved two sum with a much cleaner solution

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -66,28 +66,56 @@
  * @return {number[]}
  */
 const twoSum = (nums, target) => {
-  let sortedNums = [...nums];
-  sortedNums.sort((a, b) => {
-    return a - b;
-  });
-  let high = sortedNums.length - 1;
-  let low = 0;
-
-  for (let i = 0; i < sortedNums.length; i++) {
-    if (sortedNums[low] + sortedNums[high] === target) {
-      if (sortedNums[low] == sortedNums[high]) {
-        return [
-          nums.indexOf(sortedNums[low]),
-          nums.indexOf(sortedNums[high], nums.indexOf(sortedNums[low]) + 1),
-        ];
-      }
-      return [nums.indexOf(sortedNums[low]), nums.indexOf(sortedNums[high])];
+  const map = {};
+  for (let i = 0; i < nums.length; i++) {
+    if (map[target - nums[i]] !== undefined) {
+      return [map[target - nums[i]], i];
     }
-    if (sortedNums[low] + sortedNums[high] > target) high--;
-    if (sortedNums[low] + sortedNums[high] < target) low++;
+    map[nums[i]] = i;
   }
 };
 // @lc code=end
+// /**
+//  * @param {number[]} nums
+//  * @param {number} target
+//  * @return {number[]}
+//  */
+// const twoSum = (nums, target) => {
+//   let sortedNums = [...nums];
+//   sortedNums.sort((a, b) => {
+//     return a - b;
+//   });
+//   let high = sortedNums.length - 1;
+//   let low = 0;
+
+//   for (let i = 0; i < sortedNums.length; i++) {
+//     if (sortedNums[low] + sortedNums[high] === target) {
+//       if (sortedNums[low] == sortedNums[high]) {
+//         return [
+//           nums.indexOf(sortedNums[low]),
+//           nums.indexOf(sortedNums[high], nums.indexOf(sortedNums[low]) + 1),
+//         ];
+//       }
+//       return [nums.indexOf(sortedNums[low]), nums.indexOf(sortedNums[high])];
+//     }
+//     if (sortedNums[low] + sortedNums[high] > target) high--;
+//     if (sortedNums[low] + sortedNums[high] < target) low++;
+//   }
+// };
+
+// /**
+//  * @param {number[]} nums
+//  * @param {number} target
+//  * @return {number[]}
+//  */
+// const twoSum = (nums, target) => {
+//   const map = {};
+//   for (let i = 0; i < nums.length; i++) {
+//     const answerIndex = Object.values(map).indexOf(target - nums[i]);
+//     if (answerIndex > -1) return [answerIndex, i];
+//     map[i] = nums[i];
+//   }
+// }
 
 // The first thing that jumps out at me with this problem is that it's a lot easier if the array is sorted, after it's sorted we could easily binary search or use two pointers to find the target. I'm just not sure if there's an even faster way.
 
@@ -100,3 +128,15 @@ const twoSum = (nums, target) => {
 // 60/60 cases passed (54 ms)
 // Your runtime beats 82.96 % of javascript submissions
 // Your memory usage beats 13.97 % of javascript submissions (44.1 MB)
+
+// There's a much cleaner solution where we use a hash map that's actually a lot slower in Javascript which would be a lot quicker in most languages but Javascript doesn't have a built in way to search for values.
+
+// 60/60 cases passed (234 ms)
+// Your runtime beats 5.01 % of javascript submissions
+// Your memory usage beats 5.15 % of javascript submissions (48.1 MB)
+
+// If instead of storing the answer in the value of the hash map (which is difficult to search in Javascript) we store the answer in the key of the hash map (much faster to search) we can come up with a clean solution that's even quicker than the first one.
+
+// 60/60 cases passed (53 ms)
+// Your runtime beats 85.62 % of javascript submissions
+// Your memory usage beats 43.94 % of javascript submissions (42.8 MB)
